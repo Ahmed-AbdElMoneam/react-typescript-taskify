34d5e981631dcd355a7d116b2b50da839fa2dec4
Ahmed Mohamed AbdElMoneam <ahmed.moneam@acutebusiness.com>
2023-05-30 02:06:29 +0300

    initial commit

---
+++ src/components/InputField.tsx
@@ -0,0 +1,24 @@
+import React, { useRef } from 'react'
+import './styles.css'
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  return (
+    <form onSubmit={(e) => {
+        handleAdd(e)
+        inputRef.current?.blur();
+      }}
+      className="input">
+      <input type="input" ref={inputRef} value={todo} onChange={(e) => setTodo(e.target.value)} placeholder='Enter a task' className='input__box' />
+      <button className="input_submit" type='submit'>Go</button>
+    </form>
+  )
+}
+
+export default InputField
